9c296ce1c842899eaee94a4693d716ade82da2ff
Ryan <55410510+RyanWestwood@users.noreply.github.com>
2022-12-17 17:49:05 +0000

    added ai as dll

--- build-scripts/build-repo.bat
+++ build-scripts/build-repo.bat
@@ -1,6 +1,6 @@
 cd ../
-set working_dir=%~dp0
-set library_binaries=%working_dir%library_binaries
+set working_dir=%cd%
+set library_binaries=%working_dir%/library_binaries
 
 echo Creating Home for libs
 cd %working_dir%
@@ -46,6 +46,16 @@ cmake --install x64 --prefix %library_binaries% --config Release
 cd ../../..
 echo SDL_TTF Installed
 echo All libraries are installed
+
+echo Installing AI Library
+cd external\ai-library
+if not exist "build" mkdir build
+cd build
+cmake -G "Visual Studio 17 2022" -A x64 -DCMAKE_BUILD_TYPE=Release -S ./../ -B "x64"
+cmake --build x64 --config Release --parallel
+cmake --install x64 --prefix %library_binaries% --config Release 
+cd ../../..
+echo AI Library Installed
 
 if not exist "build" mkdir build
 cd build
